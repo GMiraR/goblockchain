d2ca9f5cbc2e1ec804c3e1108548b23ef152fe72
GMiraR <guilhermemirar@gmail.com>
2023-11-10 17:26:37 -0300

    creation of block and blockchain struct type

--- src/api/domain/blockchain.go
+++ src/api/domain/blockchain.go
@@ -1,6 +1,36 @@
 package domain
 
-import "time"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+type Blockchain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+func NewBlockchain() *Blockchain {
+	bc := new(Blockchain)
+	bc.CreateBlock(0, "Initial hash")
+	return bc
+}
+
+func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+	block := NewBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, block)
+	return block
+}
+
+func (bc *Blockchain) Print() {
+
+	for i, block := range bc.chain {
+		fmt.Printf("%s Chain %d %s \n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		block.Print()
+	}
+	fmt.Printf("%s\n", strings.Repeat("*", 60))
+}
 
 type Block struct {
 	nonce        int
@@ -15,4 +45,11 @@ func NewBlock(nonce int, previousHash string) *Block {
 	b.nonce = nonce
 	b.previousHash = previousHash
 	return b
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp      %d\n", b.timestamp)
+	fmt.Printf("nonce      %d\n", b.nonce)
+	fmt.Printf("previous_hash      %s\n", b.previousHash)
+	fmt.Printf("transactions      %s\n", b.transactions)
 }
--- src/api/main.go
+++ src/api/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"github.com/gmira/blockchain/src/api/domain"
 )
 
+func init() {
+	log.SetPrefix("Blockchain: ")
+}
+
 func main() {
-	fmt.Println("Testing go!")
+	blockchain := domain.NewBlockchain()
+	blockchain.CreateBlock(5, "hash test 1")
+	blockchain.CreateBlock(2, "hash test 2")
+	blockchain.CreateBlock(10, "hash test 3")
+	blockchain.Print()
 }
